336d44896a521925b52b5f0a56921774e95a9c2a
André Militão <112137760+andre-militao@users.noreply.github.com>
2024-11-07 10:43:11 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const menu = document.querySelector(".menu");
+
+    hamburger.addEventListener("click", () => {
+        menu.classList.toggle("open");
+    });
+});
